b7109718a21c84c4ec145740f7679db2dd7be1fc
BrayanBriones <137122666+BrayanBriones@users.noreply.github.com>
2025-07-30 15:43:45 +0000

    Completacion del taller

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import HydrologyTable from './components/HydrologyTable'
 import Student from './components/Student'
 
 // PENDIENTE: Cree la interfaz
+import { useState, useEffect } from 'react'
 import DataHour from './interface/DataHour'
 
 function App() {
@@ -12,14 +13,23 @@ function App() {
   let url = "https://raw.githubusercontent.com/aavendan/datos/refs/heads/main/CELEC/hidrologia_17122024.json"
   
   // PENDIENTE: Variable de estado y la función de modificación. 
-
+    const [data, setData] = useState<DataHour[]>([])
 
   // PENDIENTE: 
   // Realizar una petición asíncrona a la URL. La respuesta es un JSON. 
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const res = await fetch(url)
+          const json = await res.json()
+          setData(json)
+        } catch (err) {
+          console.error(err)
+        }
+      }
+      fetchData()
+    }, [])
   // Al recibir la respuesta, actualice la variable de estado.
-
-  
-
 
   return (
     <Grid container spacing={5}>
@@ -28,7 +38,7 @@ function App() {
         <Grid size={{ xs: 12 }}>
 
           {/* PENDIENTE: Envíe sus datos (apellidos, nombres y paralelo) como props del componente */}
-          <Student></Student>
+          <Student apellidos="Briones Oleas" nombres="Brayan Steven" paralelo={1}></Student>
 
         </Grid>
         
@@ -36,7 +46,7 @@ function App() {
         <Grid size={{ xs: 12 }}>
 
           {/* PENDIENTE: Envíe la variable de estado como prop */}
-          <HydrologyTable data={ }></HydrologyTable>
+          <HydrologyTable data={data}></HydrologyTable>
         
         </Grid>
         
--- src/components/Student.tsx
+++ src/components/Student.tsx
@@ -2,23 +2,20 @@ import Typography from '@mui/material/Typography';
 
 //PENDIENTE: Agregue los props apellidos, nombres y paralelo
 interface Info {
-
+    apellidos: string
+    nombres: string
+    paralelo: number
 }
 
-export default function Student( info: Info ) {
+export default function Student( { apellidos, nombres, paralelo }: Info ) {
     return (
         <>
             <Typography component="p" variant="h4">
-                
-                {/* PENDIENTE: Renderice los props apellidos y nombres */}
-                
+                {apellidos}, {nombres}
             </Typography>
             <Typography component="h2" variant="h6"
                 color="primary" gutterBottom>
-
-                {/* PENDIENTE: Renderice el paralelo */}
-                Paralelo # 
-
+                Paralelo # {paralelo}
             </Typography>
         </>
     )
